9cc3e729058f7a3f6bd7a902529033f8e7f7be04
Shruti Srivastava <113620272+shru2811@users.noreply.github.com>
2025-08-30 21:53:12 +0530

    analytics

--- src/Analytics.jsx
+++ src/Analytics.jsx
@@ -5,15 +5,17 @@ import { Analytics } from "trackcel";
 import.meta.env.VITE_TRACKCEL_PROJECT_ID
 import.meta.env.VITE_TRACKCEL_API_KEY
 
-export default function AnalyticsProvider({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <Analytics
+     <html>
+      <body>
+        {children}
+        <Analytics 
       projectId={import.meta.env.VITE_TRACKCEL_PROJECT_ID}
       apiKey={import.meta.env.VITE_TRACKCEL_API_KEY}
       debug={true}
-      disableInDevelopment={true}
-    >
-      {children}
-    </Analytics>
+    />
+      </body>
+    </html>
   );
 }
